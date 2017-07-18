09ceb31a2ad2a15b0caf6c93e12dc4bb23394dca
Fábio Souza <souzace@gmail.com>
2017-07-17 21:31:49 -0300

    clock js folder

---
+++ js/app.js
@@ -0,0 +1,12 @@
+function MyController($scope){
+    $scope.clock = new Date();
+    var updateClock = function() {
+        $scope.clock = new Date();
+    };
+
+    setInterval(function() {
+        $scope.$apply(updateClock);
+    }, 1000);
+
+    updateClock();
+}
